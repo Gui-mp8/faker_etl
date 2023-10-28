f8028ed7f425887dc3dec0d61f4c36f06f63a590
Guilherme Machado <guilherme.machado@indicium.tech>
2023-10-28 20:46:30 -0300

    chore: removing not used code and renaming files

--- src/models/conta.py
+++
@@ -1,9 +0,0 @@
-from datetime import datetime
-
-from pydantic import BaseModel
-
-class PyConta(BaseModel):
-    id: int
-    tipo: str
-    data_criacao: datetime
-    id_associado: int
--- src/models/movimento.py
+++
@@ -1,9 +0,0 @@
-from datetime import datetime
-
-from pydantic import BaseModel
-
-class PyMovimento(BaseModel):
-    vlr_transacao: float
-    des_transacao: str
-    data_movimento: datetime
-    id_cartao: int
