58ce79773d33a5b5d343369ab37c318ca38c1f4f
Guilherme Machado <guilherme.machado@indicium.tech>
2023-10-31 19:37:20 -0300

    feat: adding transformation class

--- src/main.py
+++ src/main.py
@@ -1,5 +1,6 @@
 from database.tables import PostgreSQL
 from etl.load import PostegresqlLoad
+from etl.transformation import PostgresqlTransformation
 from etl.extraction import PostgresqlExtraction
 from utils.config import load_config
 
@@ -17,8 +18,49 @@ def main(config):
     PostegresqlLoad(config).add_data_to_tables()
     print("Data Inserted!")
 
-    print("Extracting data to csv...")
-    PostgresqlExtraction(config).extract_to_csv()
+    print("Writing the tables that will be queried...")
+
+    table_name_list = [
+        "associado",
+        "conta",
+        "cartao",
+        "movimento"
+    ]
+
+    print("Quering the tables...")
+
+    query = """
+                SELECT
+                    t1.nome as nome_associado,
+                    t1.sobrenome as sobrenome_associado,
+                    t1.idade as idade_associado,
+                    t2.vlr_transacao as vlr_transacao_movimento,
+                    t2.des_transacao as des_transacao_movimento,
+                    t2.data_movimento,
+                    t3.num_cartao as numero_cartao,
+                    t3.nom_impresso as nome_impresso_cartao,
+                    t3.data_criacao as data_criacao_cartao,
+                    t4.tipo as tipo_conta,
+                    t4.data_criacao as data_criacao_conta
+                FROM
+                    associado as t1
+                JOIN
+                    movimento as t2,
+                    cartao as t3,
+                    conta as t4
+                WHERE
+                    t1.id = t4.id_associado and
+                    t4.id = t3.id_associado and
+                    t3.id = t2.id_cartao
+            """
+    print("Query Sucess!!!")
+
+    print("Writing the dataframe...")
+    df = PostgresqlTransformation(config).query_tables(table_name_list, query)
+    print("Dataframe Writed!!!")
+
+    print("Extracting dataframe to csv...")
+    PostgresqlExtraction(config).extract_to_csv(df)
     print('csv file was created in the specified path')
 if __name__ == "__main__":
     config = load_config()
